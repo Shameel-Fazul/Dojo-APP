3ffdd1df1846b86cc6ceb776f4fd4dc0e2d67973
Shameel Fazul <shameel.fazul@mail.com>
2021-03-22 16:02:40 +0530

    React App
    - Controlled Input Forms
    - Submit Events
    - POST Requests to JSON server
    - useHistory Hook
    - DELETE Requests to JSON server
    - 404 Pages

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,9 +1,16 @@
-import { useParams } from "react-router";
+import { useHistory, useParams } from "react-router";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
     const { id } = useParams(); // hook for route parameters
     const { data: blog, isPending, error } = useFetch('http://localhost:3000/blogs/' + id);
+    const history = useHistory();
+
+    const handleClick = () => {
+        fetch('http://localhost:3000/blogs/' + blog.id, {
+            method: 'DELETE'
+        }).then(() => history.push('/'))
+    }
 
 
     return ( 
@@ -15,6 +22,7 @@ const BlogDetails = () => {
                     <h2>{ blog.title }</h2>
                     <p>Written by { blog.author }</p>
                     <div>{ blog.body }</div>
+                    <button onClick={ handleClick }>delete</button>
                 </article>
             ) }
         </div>
